f28e9ec15b65156b92192efa38f2645ccdbd6865
sve-openerp <sve@openerp.com>
2014-05-23 16:58:37 +0200

    Add comment for indumat

--- bom indumat.py
+++ bom indumat.py
@@ -1,17 +1,19 @@
-bom_obj = self.pool.get("mrp.bom")
+# Create two automated actions:
+# 1- On the Stock.Move model, On creation and update, filter: [['state','=','done']]
+# 2- On the Stock.Move model, On update, before filter: [['state','=','done']], after filter:  [['state','!=','done']]
 
-# Update the Website availability of the current product
-avail = object.product_id.qty_available+object.product_id.outgoing_qty
-object.product_id.write({'x_availability':avail})
+bom_obj = self.pool.get("mrp.bom")
 
 # Find bom lines where this product is the component
 bom_line_ids = bom_obj.search(cr,uid,[('product_id','=',object.product_id.id),('bom_id','!=',False)],context=context)
 # Find parent boms of all these lines
-boms = bom_obj.browse(cr,uid,bom_obj.search(cr,uid,[('bom_lines','in',bom_line_ids)],context=context),context=context)
+boms = bom_obj.browse(cr,uid, \
+	bom_obj.search(cr,uid,[('bom_lines','in',bom_line_ids)],context=context), \
+	context=context)
 
-# Update the Website availability of the parent products
+# Update the availability of the parent boms
 for b in boms:
     avail = []
     for l in b.bom_lines:
-        avail.append(int(l.product_id.x_availability/l.product_qty))
-    b.product_id.write({'x_availability':min(avail)})
+        avail.append(int(l.product_id.qty_available/l.product_qty))
+    b.write({'x_availability':min(avail)})
