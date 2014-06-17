9caa12430042a6f6849b2639f630b6c2f3c2cb0b
sve-openerp <sve@openerp.com>
2014-06-17 22:09:18 +0200

    [IMP] NSE code review

--- tintelijn.py
+++ tintelijn.py
@@ -1,10 +1,11 @@
-bom_obj = self.pool.get("mrp.bom")
+bom_obj = self.pool["mrp.bom"]
+vals_list = []
 for l in object.order_line:
 	# Find bom lines where this product is the finished product
 	if l.product_id:
-		bom_line_ids = bom_obj.search(cr,uid,[('product_id','=',l.product_id.id),('bom_id','=',False)],context=context)
-		if len(bom_line_ids) > 0:
-			bom = bom_obj.browse(cr,uid,bom_line_ids[0],context=context)
+		bom_ids = bom_obj.search(cr,uid,[('product_id','=',l.product_id.id),('bom_id','=',False)],context=context)
+		if bom_ids:
+			bom = bom_obj.browse(cr,uid,bom_ids[0],context=context)
 			total_cost = 0.0
 			working_hours = 0.0
 			for bl in bom.bom_lines:
@@ -18,4 +19,6 @@ for l in object.order_line:
 				'x_working_hours': working_hours, 
 				'price_unit': total_cost * object.x_margin
 				}
-			l.write(vals)
+			vals_list.append((1,l.id,vals))
+if vals_list:
+	object.write({'order_line':vals_list})
