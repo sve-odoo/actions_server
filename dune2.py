f73d7c88eae0025f8ed19a957eb9da16b656ade9
sve-openerp <sve@openerp.com>
2014-07-25 11:39:12 +0200

    ADD Server action that copies a field from the SO Lines to the Invoice Lines on invoice creation

---
+++ dune2.py
@@ -0,0 +1,13 @@
+sale_obj = self.pool['sale.order']
+sol_obj = self.pool['sale.order.line']
+
+
+if object.origin:
+    sale_ids = sale_obj.search(cr,uid,[('name','=',object.origin)],context=context)
+    if sale_ids:
+        so_id = sale_ids[0]
+        sol_ids = sol_obj.search(cr,uid,[('order_id','=',so_id)],context=context)
+        sols = sol_obj.browse(cr,uid,sol_ids,context=context)
+        for sol in sols:
+            for il in sol.invoice_lines:
+                il.write({'x_activity_id':sol.x_activie and sol.x_activie.id or False})
