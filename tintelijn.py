11a3d4f43287261f7901fefff4f7c7783c139407
sve-openerp <sve@openerp.com>
2014-07-30 18:00:41 +0200

    [IMP] Tingelijn

--- tintelijn.py
+++ tintelijn.py
@@ -4,17 +4,18 @@ total_working_hours = 0.0
 for l in object.order_line:
 	# Find bom lines where this product is the finished product
 	if l.product_id:
-		bom_ids = bom_obj.search(cr,uid,[('product_id','=',l.product_id.id),('bom_id','=',False)],context=context)
+		domain = [('product_id','=',l.product_id.id),('bom_id','=',False)]
+		bom_ids = bom_obj.search(cr,uid,domain,context=context)
 		if bom_ids:
 			bom = bom_obj.browse(cr,uid,bom_ids[0],context=context)
 			total_cost = 0.0
 			working_hours = 0.0
 			for bl in bom.bom_lines:
 				if bl.product_id.type == "service":
-					total_cost += bl.product_qty * bl.product_id.standard_price * object.x_difficulty_so * l.x_difficulty_sol * object.x_transport
+					total_cost += bl.product_qty * bl.product_id.lst_price * object.x_difficulty_so * l.x_difficulty_sol * object.x_transport 
 					working_hours += bl.product_qty * object.x_difficulty_so * l.x_difficulty_sol * l.product_uom_qty
 				else:
-					total_cost += bl.product_qty * bl.product_id.standard_price * object.x_transport
+					total_cost += bl.product_qty * bl.product_id.lst_price * object.x_transport * l.x_loss_compensation_sol
 			total_working_hours += working_hours
 			vals_sol = {
 				'purchase_price': total_cost, 
